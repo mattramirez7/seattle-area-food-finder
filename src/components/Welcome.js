bf2d011c9e74f55a3cd09f6edfabc781df340950
Jeff Tu <jefftu@uw.edu>
2021-12-10 21:24:36 -0800

    Registering users with email and password

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
 export function Welcome() {
+    
+    const registerWithEmailAndPassword = async (name, email, password) => {
+        try {
+          const res = await auth.createUserWithEmailAndPassword(email, password);
+          const user = res.user;
+          await db.collection("users").add({
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+          });
+        } catch (err) {
+          console.error(err);
+          alert(err.message);
+        }
+      };
+      
     return (
         <section className="welcome">
                 <h1>Welcome to the Seattle Area Food Finder</h1>
