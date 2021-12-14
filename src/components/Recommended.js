d797e46a5c989d2a824e6dd5fe94e10262b225cd
Matthew Ramirez <matthewram38@gmail.com>
2021-12-13 20:42:01 -0800

    rendered recommended section

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -39,125 +39,125 @@ export function Recommended() {
     
 const EXAMPLE_FAV_USER_DATA = [
     {Name:"Vivi Pizzeria",Category:["Pizza", "Italian", "Gluten-Free"]},
-    {Name:"CIRCLE CHINESE CUISINE",Star:"4.5","Category":["Chinese"]},
+    {Name:"CIRCLE CHINESE CUISINE",Category:["Chinese"]},
     {Name:"Cafe Bahar",Category:["Indian"]},
     {Name:"Cafe Noir",Category:["Cafes", "Desserts", "Shaved Ice"]},
     {Name:"Good Burger",Category:["Burgers", "Fast Food", "American (Traditional)"]}    
 ]
-function chooseThreeRandom (){
-    let threeRandom = []
-    for(let i = 1; i < 4 || i == EXAMPLE_FAV_USER_DATA.lenngth; i++){
-        let random = Math.floor(Math.random()*EXAMPLE_FAV_USER_DATA.length);
-        do{
-            random = Math.floor(Math.random()*EXAMPLE_FAV_USER_DATA.length)
-        }while(threeRandom.includes(random));
-        threeRandom.push(random);
-    }
-    return(threeRandom)
+
+let threeRandom = [];
+for(let i = 1; i < 4 || i == EXAMPLE_FAV_USER_DATA.lenngth; i++){
+    let random = Math.floor(Math.random()*EXAMPLE_FAV_USER_DATA.length);
+    do{
+        random = Math.floor(Math.random()*EXAMPLE_FAV_USER_DATA.length)
+    }while(threeRandom.includes(random));
+    threeRandom.push(random);
 }
+ 
+
 
 //give back a picture name based on categoty
 function assignCategory(category){
-    if(Categories.findIndex(category) < 15){
+    if(Categories.indexOf(category) < 13){
         return("Bars.jpg")
     }
-    else if(Categories.findIndex(category)< 38){
+    else if(Categories.indexOf(category)< 36){
         return("Asian.jpg")
     }
-    else if(Categories.findIndex(category)< 48){
+    else if(Categories.indexOf(category)< 46){
         return("American.jpg")
     }
-    else if(Categories.findIndex(category)< 51){
+    else if(Categories.indexOf(category)< 49){
         return("Sushi.jpg")
     }
-    else if(Categories.findIndex(category)< 66){
+    else if(Categories.indexOf(category)< 63){
         return("LatinAmerican.jpg")
     }
-    else if(Categories.findIndex(category)< 74){
+    else if(Categories.indexOf(category)< 71){
         return("MiddleEastern.jpg")
     }
-    else if(Categories.findIndex(category)< 76){
+    else if(Categories.indexOf(category)< 73){
         return("Russian.jpg")
     }
-    else if(Categories.findIndex(category)< 79){
+    else if(Categories.indexOf(category)< 76){
         return("European.jpg")
     }
-    else if(Categories.findIndex(category)< 82){
+    else if(Categories.indexOf(category)< 79){
         return("Cafe.jpg")
     }
-    else if(Categories.findIndex(category)< 85){
+    else if(Categories.indexOf(category)< 82){
         return("Meat.jpg")
     }
-    else if(Categories.findIndex(category)< 87){
+    else if(Categories.indexOf(category)< 84){
         return("Vegan.jpg")
     }
-    else if(Categories.findIndex(category)< 89){
+    else if(Categories.indexOf(category)< 86){
         return("Grocery.jpg")
     }
-    else if(Categories.findIndex(category)< 91){
+    else if(Categories.indexOf(category)< 88){
         return("IceCream.jpg")
     }
-    else if(Categories.findIndex(category)< 94){
+    else if(Categories.indexOf(category)< 91){
         return("Soup.jpg")
     }
-    else if(Categories.findIndex(category) <95){
+    else if(Categories.indexOf(category) <92){
         return("Desserts.jpg")
     }
-    else if(Categories.findIndex(category) <96){
+    else if(Categories.indexOf(category) <93){
         return("Bakeries.jpg")
     }
-    else if(Categories.findIndex(category) <97){
+    else if(Categories.indexOf(category) <94){
         return("Pizza.jpg")
     }
-    else if(Categories.findIndex(category) <98){
+    else if(Categories.indexOf(category) <95){
         return("Burgers.jpg")
     }
-    else if(Categories.findIndex(category) <99){
-        return("Africa.jpg")
+    else if(Categories.indexOf(category) <96){
+        return("African.jpg")
     }
-    else if(Categories.findIndex(category) <100){
+    else if(Categories.indexOf(category) <97){
         return("Foodtruck.jpg")
     }
-    else if(Categories.findIndex(category) <101){
+    else if(Categories.indexOf(category) <98){
         return("ChickenWings.jpg")
     }
-    else if(Categories.findIndex(category) <102){
+    else if(Categories.indexOf(category) <99){
         return("Indian.jpg")
     }
-    else if(Categories.findIndex(category) <103){
+    else if(Categories.indexOf(category) <100){
         return("Noodles.jpg")
     }
-    else if(Categories.findIndex(category) <104){
+    else if(Categories.indexOf(category) <101){
         return("Crepes.jpg")
     }
-    else if(Categories.findIndex(category) <105){
+    else if(Categories.indexOf(category) <102){
         return("Cajin.jpg")
     }
-    else if(Categories.findIndex(category) <106){
+    else if(Categories.indexOf(category) <103){
         return("Greek.jpg")
     }
-    else if(Categories.findIndex(category) <107){
+    else if(Categories.indexOf(category) <104){
         return("Buffet.jpg")
     }
-    else if(Categories.findIndex(category) <108){
+    else if(Categories.indexOf(category) <105){
         return("Australian.jpg")
     }
-    else if(Categories.findIndex(category) <109){
+    else if(Categories.indexOf(category) <106){
         return("Smoothie.jpg")
     }
-    else if(Categories.findIndex(category) <110){
+    else if(Categories.indexOf(category) <107){
         return("Golf.jpg")
     }
-    else if(Categories.findIndex(category) <111){
+    else if(Categories.indexOf(category) <108){
         return("Lebanese.jpg")
     }
-    else if(Categories.findIndex(category) <112){
+    else if(Categories.indexOf(category) <109){
         return("FoodDelivery.jpg")
     }
-    else if(Categories.findIndex(category) <113){
-        return("Sandwhichs.jpg")
+    else if(Categories.indexOf(category) <110){
+        return("Sandwiches.jpg")
     }
-    else if(Categories.findIndex(category) <114){
+    else if(Categories.indexOf(category) <111){
         return("Fish&Chips.jpg")
     }
     
@@ -165,35 +165,23 @@ function assignCategory(category){
 
 
 
-function createReccomended(){
-       let Recommnendations = chooseThreeRandom.map((number) =>{
-        return(<div>
-           <img src={assignCategory(EXAMPLE_FAV_USER_DATA[number].Category[0])} alt={"Picture related to" +EXAMPLE_FAV_USER_DATA[number].Name}/>
-           <p>{EXAMPLE_FAV_USER_DATA[number].Name}</p>
-            </div>
-            );
-        });
-      
-   return(Recommnendations); 
-}    
+
+let Recommendations = threeRandom.map((number) =>{
+return(<div key={number}>
+        <img src={"img/"+assignCategory(EXAMPLE_FAV_USER_DATA[number].Category[0])} alt={"Picture related to" +EXAMPLE_FAV_USER_DATA[number].Name}/>
+        <p>{EXAMPLE_FAV_USER_DATA[number].Name}</p>
+    </div>
+    );
+});
+
+   
 
 
     return (
         <section className="recommended">
                 <h2>Recommended For You</h2>
                 <div className="recommendations">
-                    <div>
-                        <img src="img/foodrec1.jpg" alt="Matt's Noodle House"/>
-                        <p>Matt's Noodle House</p>
-                    </div>
-                    <div>
-                        <img src="img/foodrec2.jpg" alt="Cole's Dynamite Bistro"/>
-                        <p>Cole's Dynamite Bistro</p>
-                    </div>
-                    <div>
-                        <img src="img/foodrec3.jpg" alt="Jeff's Breakfast Eatery"/>
-                        <p>Jeff's Breakfast Eatery</p>
-                    </div>
+                    {Recommendations}
                 </div>
             </section>
     )
